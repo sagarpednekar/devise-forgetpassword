c66d08d80ab83fb1c14a9338107465b71cd5af69
Sagar Pednekar <you@example.com>
2017-10-07 12:51:28 +0530

    setup for heroku gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
-
+config.action_mailer.default_url_options = { :host => 'serene-inlet-12153.herokuapp.com' }
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
